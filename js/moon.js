7a5c69d3eeff25829b5da5777172d888287d862f
xhuanhuan <805870540@qq.com>
2016-09-21 22:22:59 +0800

    complete my sky

--- js/moon.js
+++ js/moon.js
@@ -1,7 +1,15 @@
 var c=document.getElementById("myCanvas");  
 var ctx=c.getContext("2d");  
-var radial = ctx.createRadialGradient(80,60,30,80,60,50); //重合的圆心坐标 
+
+function drawmoon(){
+var radial = ctx.createRadialGradient(100,100,30,100,100,60); //重合的圆心坐标 
 radial.addColorStop(0,'white'); 
-radial.addColorStop(0.8,'black'); 
+radial.addColorStop(1,'rgba(0,0,0,0)'); 
 ctx.fillStyle=radial;
-ctx.fillRect(0,0,160,120);
+ctx.fillRect(0,0,200,200);
+}
+
+
+
+
+
--- js/star.js
+++ js/star.js
@@ -1,103 +1,49 @@
 var c=document.getElementById("myCanvas");  
-var ctx=c.getContext("2d"); 
-
-var x=[],y=[],r=[];
-for (i=1;i<=200;i++){
-	x[i]=Math.random()*500+5;
-	y[i]=Math.random()*600+5;
-	r[i]=Math.random();
+var ctx=c.getContext("2d");  
+width=window.innerWidth;
+height=window.innerHeight;
+var starx=[],stary=[],starr=[];
+numofstar=300;
+for (i=1;i<=numofstar;i++){
+	starx[i]=Math.random()*width+5;
+	stary[i]=Math.random()*height+5;
+	starr[i]=Math.random();
 	ctx.beginPath();
-	ctx.arc(x[i], y[i], r[i], 0, 2 * Math.PI);
+	ctx.arc(starx[i], stary[i], starr[i], 0, 2 * Math.PI);
 	ctx.closePath();
 	ctx.fillStyle = 'white';
     ctx.fill();
 	
 }
 
-
-setInterval('blink()',1000);
-
 function blink(){
-	ctx.clearRect(0,0,500,600);
+	ctx.clearRect(0,0,width,height);
 	
-	var radial = ctx.createRadialGradient(80,60,30,80,60,50); //重合的圆心坐标 
-    radial.addColorStop(0,'white'); 
-    radial.addColorStop(0.8,'black'); 
-    ctx.fillStyle=radial;
-    ctx.fillRect(0,0,200,200);
-	
+    drawmoon();
 	var flag=[];
-	for(i=1;i<=200;i++){
+	for(i=1;i<=numofstar;i++){
 		flag[i]=Math.random();
 	
 	  if (flag[i]>0.5){
-		r[i]=r[i]+0.2;
-		if (r[i]>0.8)
-			r[i]=0.8;
+		starr[i]=starr[i]+0.2;
+		if (starr[i]>0.8)
+			starr[i]=0.8;
 	   }
 	  else{
-		r[i]=r[i]-0.2;
-		if(r[i]<0)
-			r[i]=0.1;
+		starr[i]=starr[i]-0.2;
+		if(starr[i]<0)
+			starr[i]=0.1;
 	  }
     }
-	for (i=1;i<=200;i++){
+	drawstar();
+}
+
+function drawstar(){
+for (i=1;i<=numofstar;i++){
 	ctx.beginPath();
-	ctx.arc(x[i], y[i], r[i], 0, 2 * Math.PI);
+	ctx.arc(starx[i], stary[i], starr[i], 0, 2 * Math.PI);
 	ctx.closePath();
 	ctx.fillStyle = 'white';
     ctx.fill();
 	}
-	
 }
-
-
-/*export default class Stars {
-    constructor(ctx, width, height, amount) {
-        this.ctx = ctx;
-        this.width = width;
-        this.height = height;
-		ctx.fillStyle = 'white';
-		 ctx.beginPath();
-        this.stars = this.getStars(amount)
-    }
-
-    getStars(amount) {
-        let stars = []
-        while (amount--) {
-            stars.push({
-                x: Math.random() * this.width,
-                y: Math.random() * this.height,
-                r: Math.random() + 0.2
-            })
-        }
-        return stars
-    }
-
-    draw() {
-        let ctx = this.ctx
-        ctx.save()
-        ctx.fillStyle = 'white'
-        this.stars.forEach(star=> {
-            ctx.beginPath()
-            ctx.arc(star.x, star.y, star.r, 0, 2 * Math.PI)
-            ctx.fill()
-        })
-        ctx.restore()
-    }
-
-    //闪烁，星星半径每隔10帧随机变大或变小
-    blink() {
-        this.stars = this.stars.map(star=> {
-            let sign = Math.random() > 0.5 ? 1 : -1
-            star.r += sign * 0.2
-            if (star.r < 0) {
-                star.r = -star.r
-            } else if (star.r > 1) {
-                star.r -= 0.2
-            }
-            return star
-        })
-
-    }
-}*/
